51e3fe4e1e53fe56744422663f9ccf2808f5a5fd
K1mjueun <jueun4779@gmail.com>
2022-07-06 11:10:08 +0900

    로그인 화면 작업중

---
+++ portfolio-bubbleroom/bubbleroom-mobile-vertical/bubbleroom-phone-vertical.js
@@ -0,0 +1,34 @@
+
+let displayAppear = [
+  {opacity: 0.7},
+  {opacity: 0.8},
+  {opacity: 0.9},
+  {opacity: 1},
+];
+
+  let displayChangeTime = {
+    duration: 500,
+    iterations: 1
+  };
+  //화면 전환 애니메이션
+
+  function displayChange(element, disappearTarget, appearTarget) {
+    element.addEventListener('click', function() {
+      disappearTarget.style.display = "none";
+      appearTarget.style.display = "flex";
+      appearTarget.animate(displayAppear,displayChangeTime);
+      appearTarget.style.opacity = 1;
+    });
+  };
+  //화면 전환 함수
+
+
+
+
+
+  //bubble room
+  const firstP = document.getElementById("first-page");
+  const logo = firstP.children[0];
+  const loginP = document.getElementById("login-page");
+
+  displayChange(logo,firstP,loginP);
